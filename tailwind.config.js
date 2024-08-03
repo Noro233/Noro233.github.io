b8803e6c277f3c352ef872e03208affde3800b81
Noro233 <araqelyan.0202@mail.ru>
2024-08-04 00:02:53 +0400

    different

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      screens: {
-        sm: '640px',
-        // => @media (min-width: 640px) { ... }
   
-        md: '768px',
-        // =>media (min-width: 768px) { ... }
-  
-        lg: '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        xl: '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
-      },
-    },
-     variants: {},
-      plugins: [],
 
       fontFamily:{
         bodyFont:["Poppins","sans-serif"] ,
@@ -41,5 +23,4 @@ module.exports = {
     plugins: [],
   }
  
-
-
+}
