1df5158f2f85d9fceca8231a402ea95ed4e58946
Noro233 <araqelyan.0202@mail.ru>
2024-08-03 23:53:51 +0400

    tailwindcss.confing change

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,26 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        sm: '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
+     variants: {},
+  plugins: [],
+
       fontFamily:{
         bodyFont:["Poppins","sans-serif"] ,
         titleFont:["Montserrat", "sans-serif"]
@@ -19,6 +39,6 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+ 
 }
 
